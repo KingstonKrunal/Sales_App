01c7c6d698dea69abff52090509545949a44d35e
GajeraDeep <deepgajera037@gmail.com>
2020-06-13 18:57:30 +0530

    Many changes

--- app/src/main/java/com/letsbiz/salesapp/HomeScreen.java
+++ app/src/main/java/com/letsbiz/salesapp/HomeScreen.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -43,7 +44,7 @@ public class HomeScreen extends AppCompatActivity {
         updateProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(HomeScreen.this, UpdateProfileActivity.class));
+                startActivity(new Intent(HomeScreen.this, profile.class));
             }
         });
 
--- app/src/main/java/com/letsbiz/salesapp/profile.java
+++ app/src/main/java/com/letsbiz/salesapp/profile.java
@@ -3,12 +3,24 @@ package com.letsbiz.salesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class profile extends AppCompatActivity {
+
+    TextView mUserNameTextView, mEmailTextView, mTotalEntriesTextView;
+    ImageButton mEditImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mUserNameTextView = findViewById(R.id.userNameTextView);
+        mEmailTextView = findViewById(R.id.emailTextView);
+        mTotalEntriesTextView = findViewById(R.id.totalEntriesTextView);
+        mEditImageButton = findViewById(R.id.editNameImageBtn);
+
+        //TODO: Update Fields in the view
     }
 }
