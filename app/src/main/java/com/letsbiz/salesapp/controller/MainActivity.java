454d23d26af355dfd016ea5102281935198a5b6c
KingstonKrunal <krunalshah661@gmail.com>
2020-06-21 18:16:59 +0530

    Added Splash Screen Activity

--- app/src/main/java/com/letsbiz/salesapp/controller/MainActivity.java
+++ app/src/main/java/com/letsbiz/salesapp/controller/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.letsbiz.salesapp.R;
 import com.letsbiz.salesapp.model.User;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,16 +15,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        if (user != null) {
-            User.setUID(user.getUid());
-            startActivity(new Intent(MainActivity.this, HomeScreen.class));
-        }
-        else {
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        }
-        finish();
+                    if (user != null) {
+                        User.setUID(user.getUid());
+                        startActivity(new Intent(MainActivity.this, HomeScreen.class));
+                    }
+                    else {
+                        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    }
+                    finish();
+                }
+            }
+        };
+
+        timerThread.start();
     }
 }
