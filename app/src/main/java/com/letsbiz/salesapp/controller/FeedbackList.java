c1dba9c2679eeccc731c13a22e76e7a4c3989331
GajeraDeep <deepgajera037@gmail.com>
2020-06-21 17:13:57 +0530

    add splash screen

--- app/src/main/java/com/letsbiz/salesapp/controller/FeedbackList.java
+++ app/src/main/java/com/letsbiz/salesapp/controller/FeedbackList.java
@@ -1,7 +1,10 @@
 package com.letsbiz.salesapp.controller;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -9,8 +12,6 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -18,82 +19,72 @@ import com.google.firebase.firestore.Query;
 import com.letsbiz.salesapp.model.Feedback;
 import com.letsbiz.salesapp.model.FirebaseConstants;
 import com.letsbiz.salesapp.R;
+import com.letsbiz.salesapp.model.User;
 
-public class FeedbackList extends AppCompatActivity {
+public class FeedbackList extends AppCompatActivity implements FeedbackListAdapter.FeedbackAdapterClickListeners {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference feedbackRef = db.collection(FirebaseConstants.FEEDBACK);
     EditFeedbackDialogFragment dialogFragment;
 
     private FeedbackListAdapter adapter;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback_list);
-        setUpRecyclerView();
 
         dialogFragment = new EditFeedbackDialogFragment();
+
+        setUpRecyclerView();
+
+        ActionBar bar = getSupportActionBar();
+        if (bar != null) {
+            bar.setTitle("Feedback List");
+        }
+    }
+
+    private Query getQueryForRecycler() {
+        Intent i = getIntent();
+        if(i.getBooleanExtra("isAdmin", false)) {
+            return feedbackRef.orderBy("date", Query.Direction.DESCENDING);
+        } else {
+            return feedbackRef.whereEqualTo("uid", User.getUID()).orderBy("date", Query.Direction.DESCENDING);
+        }
     }
 
     void setUpRecyclerView() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if(user == null) return;
-
-        Query query = feedbackRef.whereEqualTo("uid", user.getUid()).orderBy("date", Query.Direction.DESCENDING);
         FirestoreRecyclerOptions<Feedback> options = new FirestoreRecyclerOptions.Builder<Feedback>()
-                .setQuery(query, Feedback.class)
+                .setQuery(getQueryForRecycler(), Feedback.class)
                 .setLifecycleOwner(this)
                 .build();
 
         adapter = new FeedbackListAdapter(options);
 
-        RecyclerView recyclerView = findViewById(R.id.feedback_recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-        recyclerView.setAdapter(adapter);
+        mRecyclerView = findViewById(R.id.feedback_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        mRecyclerView.setAdapter(adapter);
 
-//        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
-//            @Override
-//            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
-//                return false;
-//            }
-//
-//            @Override
-//            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-//                adapter.deleteItem(viewHolder.getAdapterPosition());
-//            }
-//        }).attachToRecyclerView(recyclerView);
+//        setSlideToDelete();
 
-        adapter.setFeedbackAdapterClickListeners(new FeedbackListAdapter.FeedbackAdapterClickListeners() {
+        adapter.setFeedbackAdapterClickListeners(this);
+    }
+
+    private void setSlideToDelete() {
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
             @Override
-            public void onEditClickedListener(final DocumentSnapshot documentSnapshot, final int position) {
-                dialogFragment.setOnItemSelectedListener(new EditFeedbackDialogFragment.OnItemSelectedListener() {
-                    @Override
-                    public void deleteSelected() {
-                        adapter.deleteItem(position);
-                    }
-
-                    @Override
-                    public void editSelected() {
-                        Intent i = new Intent(FeedbackList.this, AddFeedback.class);
-                        i.putExtra(FeedbackDetails.FEEDBACK, documentSnapshot.toObject(Feedback.class));
-                        i.putExtra(FeedbackDetails.FEEDBACK_ID, documentSnapshot.getId());
-                        startActivity(i);
-                    }
-                });
-
-                dialogFragment.show(getSupportFragmentManager(), "Edit");
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
             }
 
             @Override
-            public void onViewClickedListener(DocumentSnapshot documentSnapshot, int position) {
-                Intent i = new Intent(FeedbackList.this, FeedbackDetails.class);
-                i.putExtra(FeedbackDetails.FEEDBACK, documentSnapshot.toObject(Feedback.class));
-                i.putExtra(FeedbackDetails.FEEDBACK_ID, documentSnapshot.getId());
-                startActivity(i);
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                adapter.deleteItem(viewHolder.getAdapterPosition());
             }
-        });
+        }).attachToRecyclerView(mRecyclerView);
+
     }
 
     @Override
@@ -106,5 +97,33 @@ public class FeedbackList extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         adapter.stopListening();
+    }
+
+    @Override
+    public void onEditClickedListener(final DocumentSnapshot documentSnapshot, final int position) {
+        dialogFragment.setOnItemSelectedListener(new EditFeedbackDialogFragment.OnItemSelectedListener() {
+            @Override
+            public void deleteSelected() {
+                adapter.deleteItem(position);
+            }
+
+            @Override
+            public void editSelected() {
+                Intent i = new Intent(FeedbackList.this, AddFeedback.class);
+                i.putExtra(FeedbackDetails.FEEDBACK, documentSnapshot.toObject(Feedback.class));
+                i.putExtra(FeedbackDetails.FEEDBACK_ID, documentSnapshot.getId());
+                startActivity(i);
+            }
+        });
+
+        dialogFragment.show(getSupportFragmentManager(), "Edit");
+    }
+
+    @Override
+    public void onViewClickedListener(DocumentSnapshot documentSnapshot, int position) {
+        Intent i = new Intent(FeedbackList.this, FeedbackDetails.class);
+        i.putExtra(FeedbackDetails.FEEDBACK, documentSnapshot.toObject(Feedback.class));
+        i.putExtra(FeedbackDetails.FEEDBACK_ID, documentSnapshot.getId());
+        startActivity(i);
     }
 }
